8458266e6cf3089ef925ec0cc645179300c3c403
acbst0 <125031376+acbst0@users.noreply.github.com>
2023-11-01 18:07:45 +0300

    01.11.2023 | 6:08

---
+++ printf/ft_print_num.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_num.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abostano <abostano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 09:35:47 by abostano          #+#    #+#             */
+/*   Updated: 2023/11/01 16:19:55 by abostano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_num(int i)
+{
+	char	*num;
+	int	len;
+
+	num = ft_itoa(i);
+	len = ft_printstr(num);
+	free(num);
+	return (len);
+}
+
+int	ft_unsigned(unsigned int i)
+{
+	
+}
+
+int	ft_printhex_up(long int i)
+{
+	
+}
--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abostano <abostano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 11:41:59 by abostano          #+#    #+#             */
-/*   Updated: 2023/10/31 14:38:55 by abostano         ###   ########.fr       */
+/*   Updated: 2023/11/01 17:52:26 by abostano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,23 @@ int	ft_printchar(int c)
 
 int	ft_types(va_list args, const char *str)
 {
-	
+	int	len;
+
+	len = 0;
+	if (str == 'c')
+		len += ft_printchar(va_arg(args, char));
+	else if (str == 's')
+		len += ft_printstr(va_arg(args, char *));
+	else if (str == 'p')
+		len += 
+	else if (str == 'd' || str == 'i')
+		len += ft_print_num(va_arg(args, int));
+	else if (str == 'u')
+		len += ft_unsigned(va_arg(args, unsigned int));
+	else if (str == 'x')
+		len += ft_print_hex(va_arg(args, int))
+	else if (str == 'X')
+		len += 
 }
 
 int	ft_printf(const char *input, ...)
@@ -37,11 +53,12 @@ int	ft_printf(const char *input, ...)
 	{
 		if (input[a] == '%')
 		{
-			len += 
+			len += ft_types(arg, input[a + 1]);
+			a++;
 		}
 		else
-			ft_printchar(input[a]);
+			len += ft_printchar(input[a]);
+		a++;
 	}
-	
 	va_end(arg);
 }
